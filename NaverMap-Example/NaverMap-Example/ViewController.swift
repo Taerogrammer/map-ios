3fd55c8ffcc14bf70184453616c1727724e1b8ea
Taerogrammer <hyung54321@naver.com>
2023-09-19 01:59:10 +0900

    네이버지도 프로젝트 생성

---
+++ NaverMap-Example/NaverMap-Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NaverMap-Example
+//
+//  Created by 김태형 on 2023/09/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
